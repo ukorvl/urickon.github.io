f04f916cfb6f9920d0177bff0f3c7eaaf607d5c7
Urickon <Rodoslavak@yandex.ru>
2021-09-05 17:37:15 +0300

    implement sending form

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,46 +1,66 @@
-/** Disables button while inputs are empty */
+---
+---
 
-const nameInput = document.querySelector('#name');
-const messageInput = document.querySelector('#message');
-const submitButton = document.querySelector('#sbtBtn');
-submitButton.classList.add('button-disabled');
-let processingRequest = false;
+const
+  nameInput = document.querySelector('#name'),
+  messageInput = document.querySelector('#message'),
+  form = document.getElementById('form'),
+  submitButton = document.querySelector('#sbtBtn'),
+  inputs = document.querySelectorAll('.comment-textfield');
 
-const inputs = document.querySelectorAll('.comment-textfield');
+let processingRequest = false; // true during request submitting
+let error = false; // true if submitting vas unsuccesfull
 
 const checkInputs = () => {
   [...inputs].some(input => input.value === '')
     ? submitButton.classList.add('button-disabled')
     : submitButton.classList.remove('button-disabled');
 }
-
 checkInputs();
 
+/** Disables button while inputs are empty */
 inputs.forEach(node => {
   node.addEventListener('keyup', () => {
+    if (error) error = false
     checkInputs();
   })
 })
 
-submitButton.addEventListener('click', (e) => {
-  inputs.forEach(input => !input.value && e.preventDefault());
-});
-
+form.addEventListener( "submit", function ( event ) {
+  event.preventDefault();
+  if ([...inputs].some(input => input.value === '') || processingRequest) { return; }
+  processForm();
+} );
 
 /** Onsubmit */
-function processForm(e) {
-  console.log('submit');
-  submitButton.classList.add('button-disabled')
-  // inputs.forEach(input => input.value = '')
+async function processForm () {
+  const FD = new FormData(form);
+  processingRequest = true;
+  submitButton.classList.add('button-disabled');
+
+  let body = '';
+  for (const key of FD.keys()) {
+    body = body + `${key}=` + encodeURIComponent(FD.get(key)) + '&'
+  };
+  body = body.slice(0, -1);
+  
+  const XHR = new XMLHttpRequest();
+  XHR.addEventListener( "load", function(event) {
+    inputs.forEach(input => input.value = '');
+    processingRequest = false;
+  });
+  XHR.addEventListener( "error", function(event) {
+    error = true;
+    processingRequest = false;
+  });
+  XHR.open("POST", "{{ site.staticman_url }}", true);
+  XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  XHR.send(body);
 };
 
-const form = document.getElementById('form');
-if (form.attachEvent) {
-  form.attachEvent("submit", processForm);
-} else {
-  form.addEventListener("submit", processForm);
-};
-
+/**
+ * Corrects window scroll offset after clicking on permalink .
+ */
 function correctPermalink () {
   setTimeout(() => {
     (window.innerHeight + window.pageYOffset) < document.body.offsetHeight && window.scrollBy(0, -76);
